0c744b6e11a646829aef2bc13242d1f4bb03aaac
waelhassine <wael.hassine0@gmail.com>
2022-02-12 23:28:56 +0100

    Fixed Issue

--- components/SectionOneHomePage.js
+++ components/SectionOneHomePage.js
@@ -2,6 +2,9 @@ import { FaHashtag } from "react-icons/fa";
 import Image from "next/image";
 import Image1 from "../public/image_01-6.jpg";
 import Image2 from "../public/image_01-5.jpg";
+import Image3 from "../public/image_10-1.jpg";
+import Image4 from "../public/image_10-2.jpg";
+import Image5 from "../public/image_03-2.jpg";
 const Contents = () => {
   return (
     <div className="container mx-auto py-24  space-y-20 w-full">
@@ -42,13 +45,13 @@ const Contents = () => {
         </div>
         <div className="grid grid-cols-2 grid-rows-2 gap-2">
           <div className="col-span-1 row-span-2">
-            <img src="/image_10-1.jpg" />
+            <Image src={Image3} alt="Picture of the author" />
           </div>
           <div className="col-span-1 row-span-1">
-            <img src="/image_10-2.jpg" />
+            <Image src={Image4} alt="Picture of the author" />
           </div>
           <div className="col-span-1 row-span-1">
-            <img src="/image_03-2.jpg" />
+            <Image src={Image5} alt="Picture of the author" />
           </div>
         </div>
       </div>
